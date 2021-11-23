9aac25e14e0dfc62a419dd173feed81370335ac3
bitowaqr <bitowaqr@gmail.com>
2021-11-23 03:14:43 +0100

    include index

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 fetch("links.json")
     .then(response => response.json())
     .then(function (parsedData) {
-        
+
         var items = document.querySelectorAll(".item");
 
-    for (let i = 0; i < parsedData.length; i++) {
-        items[i].innerHTML = parsedData[i].title;
-        items[i].href = parsedData[i].url;
-        items[i].target = "_blank";
-        items[i].classList.add("available");
+        for (let i = 0; i < parsedData.length; i++) {
+        index_i = parsedData[i].index -1;
+        items[index_i].innerHTML = parsedData[i].title;
+        items[index_i].href = parsedData[i].url;
+        items[index_i].target = "_blank";
+        items[index_i].classList.add("available");
     }
 
 })
